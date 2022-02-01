2b95c8653912bb929629cfc988531034c08c7d75
Thomas Beckford <tebeckford@gmail.com>
2022-02-01 16:16:30 -0300

    feat: adding husky

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'abc'],
+  },
 }
 
 module.exports = nextConfig
